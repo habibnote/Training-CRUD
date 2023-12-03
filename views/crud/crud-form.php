3ac01dfd8e340fc98bf8eabd7a95be74a0163110
Habibur Rahman <habibblog97@gmail.com>
2023-12-03 20:47:37 +0600

    crud markup done

--- views/crud/crud-form.php
+++ views/crud/crud-form.php
@@ -1,6 +1,8 @@
 <div class="tc-admin-container">
     <h2>Year 2023</h2>
     <div class="tc-admin-table-warpper">
+
+        <p class="tc-addnew"><a href="">Add New Item</a></p>
         <table>
             <thead>
                 <tr>
@@ -10,6 +12,7 @@
                     <th>Department</th>
                     <th>Program</th>
                     <th>Max no. of participants</th>
+                    <th>Action</th>
                 </tr>
             </thead>
             <tbody>
@@ -20,6 +23,11 @@
                     <td>Department</td>
                     <td>Program</td>
                     <td>Max no. of participants</td>
+                    <td>
+                        <a href="#">Edit</a> |
+                        <a href="#">Update</a> |
+                        <a href="#">Delete</a>
+                    </td>
                 </tr>
             </tbody>
         </table>
