80f950a384fa3463432a858d6a5603424f4b92f3
Habibur Rahman <habibblog97@gmail.com>
2023-12-03 21:06:32 +0600

    condiitonally add page added

--- views/crud/crud-form.php
+++ views/crud/crud-form.php
@@ -6,41 +6,52 @@
         // Get the post slug
         $current_slug = $post->post_name;
 
-        $add_new_url    = add_query_arg( 'tc_add', 'true', site_url( $current_slug ) );
+        $add_new_url    = add_query_arg( 'tc_task', 'add', site_url( $current_slug ) );
     }
+
+    $tc_task = $_GET['tc_task'] ?? '';
+
 ?>
 <div class="tc-admin-container">
     <h2><?php printf( "%s %s", esc_html__( 'Year' ), date("Y") ); ?></h2>
     <div class="tc-admin-table-warpper">
 
         <p class="tc-addnew"><a href="<?php echo esc_url( $add_new_url ); ?>"><?php esc_html_e( 'Add New Item' ); ?></a></p>
-        <table>
-            <thead>
-                <tr>
-                    <th>Month</th>
-                    <th>Start Date</th>
-                    <th>End Date</th>
-                    <th>Department</th>
-                    <th>Program</th>
-                    <th>Max no. of participants</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Month</td>
-                    <td>Start Date</td>
-                    <td>End Date</td>
-                    <td>Department</td>
-                    <td>Program</td>
-                    <td>Max no. of participants</td>
-                    <td>
-                        <a href="#">Edit</a> |
-                        <a href="#">Update</a> |
-                        <a href="#">Delete</a>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        <?php 
+            if( isset( $tc_task ) && 'add' == $tc_task ) {
+                echo "Hello";
+            }else{
+                ?>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Month</th>
+                            <th>Start Date</th>
+                            <th>End Date</th>
+                            <th>Department</th>
+                            <th>Program</th>
+                            <th>Max no. of participants</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Month</td>
+                            <td>Start Date</td>
+                            <td>End Date</td>
+                            <td>Department</td>
+                            <td>Program</td>
+                            <td>Max no. of participants</td>
+                            <td>
+                                <a href="#">Edit</a> |
+                                <a href="#">Update</a> |
+                                <a href="#">Delete</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <?php
+            }
+        ?>
     </div>
 </div>
