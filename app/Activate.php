74a6dc486f2c47eae8b2f290d4b7a3d35e5c8fb2
Habibur Rahman <habibblog97@gmail.com>
2023-12-03 22:27:35 +0600

    wpdb table created success

--- app/Activate.php
+++ app/Activate.php
@@ -18,6 +18,36 @@ class Activate {
      * this function run when plugin is activate
      */
     public function activate() {
+        global $wpdb;
+
+        //table name
+        $table_name = $table_name = $wpdb->prefix . 'tc_habib';
         
+        // Get the default character set and collation for the database
+        $charset_collate = $wpdb->get_charset_collate();
+
+        /**
+         * Check table already exist or not
+         */
+        if( ! is_table_exists( $table_name ) ) {
+
+            // Define the table structure
+            $sql = "CREATE TABLE $table_name (
+                id INT NOT NULL AUTO_INCREMENT,
+                tc_month VARCHAR(255) NOT NULL,
+                tc_start_date VARCHAR(255) NOT NULL,
+                tc_end_date VARCHAR(255) NOT NULL,
+                tc_depart VARCHAR(255) NOT NULL,
+                tc_program VARCHAR(255) NOT NULL,
+                tc_number VARCHAR(255) NOT NULL,
+                PRIMARY KEY (id)
+            ) $charset_collate;";
+
+            // Include the upgrade file for dbDelta
+            require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+
+            // Run dbDelta
+            dbDelta($sql);
+        }
     }
 }
