cb8c2cb36b8995f128036230b6df61b752665592
Habibur Rahman <habibblog97@gmail.com>
2023-12-03 20:39:29 +0600

    converted it as a table

--- views/crud/crud-form.php
+++ views/crud/crud-form.php
@@ -1,21 +1,27 @@
 <div class="tc-admin-container">
     <h2>Year 2023</h2>
     <div class="tc-admin-table-warpper">
-        <div class="tc-table-heading">
-            <div class="tc-th">Month</div>
-            <div class="tc-th">Start Date</div>
-            <div class="tc-th">End Date</div>
-            <div class="tc-th">Department</div>
-            <div class="tc-th">Program</div>
-            <div class="tc-th">Max no. of participants</div>
-        </div>  
-        <div class="tc-table-heading">
-            <div class="tc-td">Month</div>
-            <div class="tc-td">Start Date</div>
-            <div class="tc-td">End Date</div>
-            <div class="tc-td">Department</div>
-            <div class="tc-td">Program</div>
-            <div class="tc-td">Max no. of participants</div>
-        </div> 
+        <table>
+            <thead>
+                <tr>
+                    <th>Month</th>
+                    <th>Start Date</th>
+                    <th>End Date</th>
+                    <th>Department</th>
+                    <th>Program</th>
+                    <th>Max no. of participants</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Month</td>
+                    <td>Start Date</td>
+                    <td>End Date</td>
+                    <td>Department</td>
+                    <td>Program</td>
+                    <td>Max no. of participants</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </div>
