42a8b5b08f4eb8380b52d35e124741e751733223
Habibur Rahman <habibblog97@gmail.com>
2023-12-03 20:30:22 +0600

    all assets loaded

--- app/Shortcode.php
+++ app/Shortcode.php
@@ -11,6 +11,7 @@ class Shortcode {
      * Class constructor
      */
     public function __construct() {
+        add_action( 'wp_enqueue_scripts', [$this, 'enqueue_scripts'] );
         add_shortcode( 'tc_crud', [$this, 'tc_crud'] );
     }
 
@@ -22,5 +23,16 @@ class Shortcode {
         include_once( TR_CRUD_DIR . "/views/crud/crud-form.php" );
     }
 
+    /**
+     * Load all assets
+     */
+    public function enqueue_scripts() {
 
+        //Only for front
+        if( ! is_admin() ) {
+            
+            wp_enqueue_style( 'front-css', TR_CRUD_ASSET . '/front/css/front.css', [], time(), 'all'  );
+            wp_enqueue_script( 'front-js', TR_CRUD_ASSET . '/front/js/front.js', ['jquery'], time(), true );
+        }
+    }
 }
