9d48a9e91694984c844c108e3429cc7dfeee7e31
Habibur Rahman <habibblog97@gmail.com>
2023-12-03 22:12:05 +0600

    activate function added

--- app/Activate.php
+++ app/Activate.php
@@ -11,6 +11,13 @@ class Activate {
      * Class contructor
      */
     public function __construct() {
+        register_activation_hook( TR_CRUD, [$this, 'activate'] );
+    }
+
+    /**
+     * this function run when plugin is activate
+     */
+    public function activate() {
         
     }
 }
