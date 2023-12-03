b2696094ccd2662804ab5997a7f8c819700186ae
Habibur Rahman <habibblog97@gmail.com>
2023-12-03 20:12:07 +0600

    crud shortcode added

--- app/Shortcode.php
+++ app/Shortcode.php
@@ -2,9 +2,24 @@
 
 namespace Habib\TrainingCrud\App;
 
+/**
+ * Shortcode class
+ */
 class Shortcode {
 
+    /**
+     * Class constructor
+     */
     public function __construct() {
-        
+        add_shortcode( 'tc_crud', [$this, 'tc_crud'] );
     }
+
+    /**
+     * Main CRUD shortcode
+     */
+    public function tc_crud() {
+        echo "Hello";
+    }
+
+
 }
