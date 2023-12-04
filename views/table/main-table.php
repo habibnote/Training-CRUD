1755ae9066b802aad7e25250fce19b533577e1e9
Habibur Rahman <habibblog97@gmail.com>
2023-12-04 20:30:55 +0600

    table header done

--- views/table/main-table.php
+++ views/table/main-table.php
@@ -1,3 +1,18 @@
-<?php 
-
-echo "Hello";
+<div class="tc_view_table">
+    <table>
+        <thead>
+            <tr>
+                <th rowspan="2">S1 No.</th>
+                <th rowspan="2">Month & Year</th>
+                <th colspan="2">Date</th>
+                <th rowspan="2">No of Dates</th>
+                <th rowspan="2">Title of the Traning/Workshop</th>
+                <th rowspan="2">Max. No. Participants</th>
+            </tr>
+            <tr>
+                <th>Form</th>
+                <th>To</th>
+            </tr>
+        </thead>
+    </table>
+</div>
