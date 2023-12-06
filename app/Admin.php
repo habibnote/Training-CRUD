01a581bc0f08dff09bd5079bacd3816be95c86da
Habibur Rahman <habibblog97@gmail.com>
2023-12-06 21:49:43 +0600

    admin script added

--- app/Admin.php
+++ app/Admin.php
@@ -11,8 +11,20 @@ class Admin {
      * class constructor
      */
     public function __construct() {
-        
+
+        add_action( 'admin_enqueue_scripts', [$this, 'admin_enqueue_scripts'] );
         add_action( 'admin_menu' , [$this, 'add_menuPage'] );
+    }
+
+    /**
+     * Admin script
+     */
+    public function admin_enqueue_scripts() {
+        wp_enqueue_style( 'admin-css', TR_CRUD_ASSET . '/admin/css/admin.css', [], time(), 'all'  );
+        wp_enqueue_script( 'admin-js', TR_CRUD_ASSET . '/admin/js/admin.js', ['jquery'], time(), true );
+
+        $ajax_url = admin_url( 'admin-ajax.php' );
+        wp_localize_script( 'admin-js', 'TC_ADMIN', array( 'ajax' => $ajax_url ) );
     }
 
     /**
