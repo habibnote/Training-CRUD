61756eb923278038548b8a6280f04c424e38e8b8
Habibur Rahman <habibblog97@gmail.com>
2023-12-06 23:00:25 +0600

    add new table done

--- views/form/add-new.php
+++ views/form/add-new.php
@@ -43,56 +43,70 @@
 ?>
 
 <form method="POST">
-    <label for="tc_month">Month:</label>
-    <select id="tc_month" name="tc_month" required>
-        <option value="" disabled selected>Select Month</option>
-        <option value="January">January</option>
-        <option value="February">February</option>
-        <option value="March">March</option>
-        <option value="April">April</option>
-        <option value="May">May</option>
-        <option value="June">June</option>
-        <option value="July">July</option>
-        <option value="August">August</option>
-        <option value="September">September</option>
-        <option value="October">October</option>
-        <option value="November">November</option>
-        <option value="December">December</option>
-    </select>
+    <p>
+        <label for="tc_month">Month:</label>
+        <select id="tc_month" name="tc_month" required>
+            <option value="" disabled selected>Select Month</option>
+            <option value="January">January</option>
+            <option value="February">February</option>
+            <option value="March">March</option>
+            <option value="April">April</option>
+            <option value="May">May</option>
+            <option value="June">June</option>
+            <option value="July">July</option>
+            <option value="August">August</option>
+            <option value="September">September</option>
+            <option value="October">October</option>
+            <option value="November">November</option>
+            <option value="December">December</option>
+        </select>
+    </p>
     
-    <label for="tc_start_date">Start Date: </label>
-    <input type="date" value="06/12/2023" class="tc-date-format" name="tc_start_date" id="tc_start_date" required>
+    <p>
+        <label for="tc_start_date">Start Date: </label>
+        <input type="date" value="06/12/2023" class="tc-date-format" name="tc_start_date" id="tc_start_date" required>
+    </p>
 
-    <label for="tc_end_date">Start Date: </label>
-    <input type="date" name="tc_end_date" class="tc-date-format" id="tc_end_date" required>
+    <p>
+        <label for="tc_end_date">Start Date: </label>
+        <input type="date" name="tc_end_date" class="tc-date-format" id="tc_end_date" required>
+    </p>
 
-    <label for="tc_depart">Department:</label>
-    <select name="tc_depart" id="tc_depart" required>
-        <?php 
-            $tc_depart_setting = get_option( 'tc_depart_setting' );
-            $depart_array = explode( "\n", $tc_depart_setting );
-            foreach( $depart_array as $value ) {
-                printf( "<option value='%s'>%s</option>", $value, $value); 
-            }
-        ?>
-    </select>
+    <p>
+        <label for="tc_depart">Department:</label>
+        <select name="tc_depart" id="tc_depart" required>
+            <?php 
+                $tc_depart_setting = get_option( 'tc_depart_setting' );
+                $depart_array = explode( "\n", $tc_depart_setting );
+                foreach( $depart_array as $value ) {
+                    printf( "<option value='%s'>%s</option>", $value, $value); 
+                }
+            ?>
+        </select>
+    </p>
 
-    <label for="tc_program">Program:</label>
-    <select name="tc_program" id="tc_program" required>
-        <?php 
-            $tc_program_setting = get_option( 'tc_program_setting' );
-            $program_array = explode( "\n", $tc_program_setting );
-            foreach( $program_array as $value ) {
-                printf( "<option value='%s'>%s</option>", $value, $value); 
-            }
-        ?>
-    </select>
+    <p>
+        <label for="tc_program">Program:</label>
+        <select name="tc_program" id="tc_program" required>
+            <?php 
+                $tc_program_setting = get_option( 'tc_program_setting' );
+                $program_array = explode( "\n", $tc_program_setting );
+                foreach( $program_array as $value ) {
+                    printf( "<option value='%s'>%s</option>", $value, $value); 
+                }
+            ?>
+        </select>
+    </p>
 
-    <label for="tc_number">Max no. of participants:</label>
-    <input type="text" name="tc_number" id="tc_number" required>
+    <p>
+        <label for="tc_number">Max no. of participants:</label>
+        <input type="text" name="tc_number" id="tc_number" required>
+    </p>
 
     <br><br>
 
-    <button type="submit" name="tc_submit">Add</button>
+    <p>
+        <button type="submit" name="tc_submit">Add</button>
+    </p>
   
 </form>
